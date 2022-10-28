ecc3b6f6bd1bf3d23f81da1387e022300a46b220
Husain Nazer <husainanazer@gmail.com>
2022-10-28 09:20:25 +0530

    added loading state

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
 export default function Home() {
+    const [loading, setLoading] = useState(true)
     const [btcPrice, setBtcPrice] = useState(0)
 
     useEffect(() => {
@@ -13,7 +14,9 @@ export default function Home() {
         const res = await fetch('/api/current-price')
         const data = await res.json()
         const btcPriceRounded = Math.round(data.rate)
+
         setBtcPrice(btcPriceRounded)
+        setLoading(false)
     }
 
     return (
@@ -27,7 +30,7 @@ export default function Home() {
             <main className="bg-gradient-to-br from-orange-500 to-yellow-300">
                 <div className="flex justify-center items-center h-screen">
                     <h1 className="text-9xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-gray-700 to-gray-900">
-                        ${btcPrice}
+                        {!loading ? `$${btcPrice}` : '...'}
                     </h1>
                 </div>
             </main>
